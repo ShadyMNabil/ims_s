f4f45de9712b437850d93e1d749a859a7f0c3097
ShadyMNabil <shady.muhammad.nabil@gmail.com>
2025-01-18 16:50:25 +0200

    Update web routes with Admin prefix

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'index')
     ->name('index');
 
-Route::view('/dashboard', 'admins.dashboard')
-    ->name('admins.dashboard');
+Route::name('admins.')
+    ->prefix('dashboard')
+    ->group(function () {
+        Route::view('/', 'admins.dashboard')
+            ->name('dashboard');
+    });
